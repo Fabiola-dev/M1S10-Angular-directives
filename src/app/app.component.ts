0c0a66f6c612629213965fbeffdd524f25e79785
Fabiola Magalhaes <fabimagal99@gmail.com>
2023-05-09 17:00:44 -0300

    structuring project - second commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'semana10';
-
-  ngOnInit(): void {
-
-  }
-
-  onMode = true;
-
+export class AppComponent {
   
+  photos = [
+    {
+      url: 'https://thumbs.dreamstime.com/z/zoo-animals-21990181.jpg',
+      description: 'Zoo animals',
+    },
+    {
+      url: 'https://thumbs.dreamstime.com/z/zoo-animals-21990181.jpg',
+      description: 'Zoo animals',
+    },
+    {
+      url: 'https://thumbs.dreamstime.com/z/zoo-animals-21990181.jpg',
+      description: 'Zoo animals',
+    },
+    {
+      url: 'https://thumbs.dreamstime.com/z/zoo-animals-21990181.jpg',
+      description: 'Zoo animals',
+    },
+    {
+      url: 'https://thumbs.dreamstime.com/z/zoo-animals-21990181.jpg',
+      description: 'Zoo animals',
+    },
+  ]
+  
+
   
   
 }
